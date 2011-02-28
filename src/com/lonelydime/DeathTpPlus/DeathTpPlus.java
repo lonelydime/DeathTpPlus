96a435af4c4067489eed209336dd2c797806547c
lonelydime <lonelydime@gmail.com>
2011-02-28 14:37:57 -0500

    updated to use onCommand

--- src/com/lonelydime/DeathTpPlus/DeathTpPlus.java
+++ src/com/lonelydime/DeathTpPlus/DeathTpPlus.java
@@ -1,13 +1,22 @@
 package com.lonelydime.DeathTpPlus;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.Event.Priority;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
@@ -22,7 +31,6 @@ import org.anjocaido.groupmanager.GroupManager;
 
 public class DeathTpPlus extends JavaPlugin{
 	private final DTPEntityListener entityListener = new DTPEntityListener(this);
-	private final DTPPlayerListener playerListener = new DTPPlayerListener(this);
 	
 	private final Logger log = Logger.getLogger("Minecraft");
     public static HashMap<String, List<String>> killstreak = new HashMap<String, List<String>>();
@@ -124,10 +132,6 @@ public class DeathTpPlus extends JavaPlugin{
 	        //pm.registerEvent(Event.Type.ENTITY_DAMAGEDBY_ENTITY, entityListener, Priority.Normal, this);
         }
         
-        if (DeathTpPlus.deathconfig.get("ALLOW_DEATHTP").equals("true") || DeathTpPlus.deathconfig.get("SHOW_STREAKS").equals("true") ) {
-        	pm.registerEvent(Event.Type.PLAYER_COMMAND, this.playerListener, Event.Priority.Normal, this);
-        }
-        
         setupPermissions();
 
         PluginDescriptionFile pdfFile = this.getDescription();
@@ -150,5 +154,185 @@ public class DeathTpPlus extends JavaPlugin{
             }
             gm = (GroupManager) p;
         } 
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		String command = cmd.getName();
+		boolean canUseCommand = true;
+		boolean teleportok = true;
+		
+		if (command.equals("deathtp")) {
+			if (sender instanceof Player) {
+				Player player = (Player)sender;
+				
+				if (Permissions != null) {
+					canUseCommand = Permissions.has(player, "deathtpplus.deathtp");
+				}
+				else if (gm != null) {
+					canUseCommand = gm.getHandler().has(player, "deathtpplus.deathtp");
+				}
+				else {
+					canUseCommand = deathconfig.get("ALLOW_DEATHTP").equals("true");
+				}
+				
+				if (canUseCommand) {
+					if (!deathconfig.get("CHARGE_ITEM_ID").equals("0") ) {
+						if (player.getItemInHand().getType().getId() != Integer.parseInt(deathconfig.get("CHARGE_ITEM_ID"))) {
+							player.sendMessage("You must be holding a "+Material.getMaterial(Integer.parseInt(deathconfig.get("CHARGE_ITEM_ID"))).toString()+" to teleport.");
+							teleportok = false;
+						}
+						else {
+							ItemStack currentitem = player.getItemInHand();
+							int itemnum = currentitem.getAmount();
+							itemnum--;
+							if (itemnum > 0) {
+								currentitem.setAmount(itemnum);
+								player.setItemInHand(currentitem);
+							}
+							else {
+								player.getInventory().clear(player.getInventory().getHeldItemSlot());
+							}
+						}
+					}
+					if (teleportok) {
+						File fileName = new File("plugins/DeathTpPlus/locs.txt");
+						try {
+							String line = "";
+							String teleloc = "";
+							String[] location;
+							FileReader fr = new FileReader(fileName);
+							BufferedReader br = new BufferedReader(fr);
+							
+							while((line = br.readLine()) != null) {
+								if (line.contains(player.getName()+":")) {
+									teleloc = line;
+								}	
+							}
+							
+							if (teleloc != "") {
+								location = teleloc.split(":");
+								Location sendLocation = player.getLocation();
+								sendLocation.setX(Double.valueOf(location[1].trim()).doubleValue());
+								sendLocation.setY(Double.valueOf(location[2].trim()).doubleValue());
+								sendLocation.setZ(Double.valueOf(location[3].trim()).doubleValue());
+								player.teleportTo(sendLocation);
+							}
+							else {
+								player.sendMessage("You do not have a last known death location.");
+							}
+							
+						}
+						catch (IOException e) {
+			
+						}
+					}
+					else {
+						player.sendMessage("That command is not available");
+					}
+				
+				}
+
+				return true;
+			}
+			
+			else {
+				System.out.println("This is only a player command.");
+				return true;
+			}
+			
+		}
+		
+		else if (command.equals("suicide")) {
+			if (sender instanceof Player) {
+				Player player = (Player)sender;
+				player.setHealth(0);
+			}
+			else {
+				System.out.println("This is only a player command.");
+				return true;
+			}
+		}
+		
+		else if (command.equals("streak")) {
+			canUseCommand = true;
+			
+			if (sender instanceof Player) {
+				if (Permissions != null) {
+					canUseCommand = Permissions.has((Player)sender, "deathtpplus.streak");
+				}
+				else if (DeathTpPlus.gm != null) {
+					canUseCommand = DeathTpPlus.gm.getHandler().has((Player)sender, "deathtpplus.streak");
+				}
+			}
+				
+			if (canUseCommand) {
+				if (DeathTpPlus.deathconfig.get("SHOW_STREAKS").equals("true") ) {
+					File streakFile = new File("plugins/DeathTpPlus/streak.txt");
+					String line;
+					String[] splittext;
+					Player check;
+					String playername = "player";
+					
+					if (args.length > 1) {
+						playername = args[0];
+					}
+					else {
+						if (sender instanceof Player) {
+							check = (Player)sender;
+							playername = check.getName();
+						}
+					}
+					
+					List<Player> lookup = this.getServer().matchPlayer(playername);
+					
+					if (lookup.size() == 0) {
+						sender.sendMessage(ChatColor.RED+ "No matching player.");
+						return true;
+					} 
+					else if (lookup.size() != 1) {
+						sender.sendMessage(ChatColor.RED+ "Matched more than one player!  Be more specific!");
+						return true;
+					} 
+					else {
+						check = lookup.get(0);
+						
+						try {
+							FileReader fr = new FileReader(streakFile);
+							BufferedReader br = new BufferedReader(fr);
+							boolean entryfound = false;
+							while((line = br.readLine()) != null) {
+								if (!line.startsWith("#")) {
+									splittext = line.split(":");
+									if (check.getName().matches(splittext[0])) {
+										if (Integer.parseInt(splittext[1]) < 0) {
+											sender.sendMessage(ChatColor.GRAY+check.getName()+"'s Current Streak: "+splittext[1].replace("-", "")+" Death(s)");
+										}
+										else {
+											sender.sendMessage(ChatColor.GRAY+check.getName()+"'s Current Streak: "+splittext[1]+" Kill(s)");
+										}
+										
+										entryfound = true;
+									}
+								}
+							}
+							if (!entryfound) {
+								sender.sendMessage("No streak found");
+							}
+							br.close();
+							return true;
+						}
+						catch (IOException e) {
+							System.out.println(e);
+						}
+					}
+					
+					
+				}
+			}
+
+		}
+		
+		
+		return false;
 	}
 }
